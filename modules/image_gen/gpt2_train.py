dc92002f46cd7bc5c5a7e155c757aaba60334242
Tianyu FAN <tian-yu.fan@connect.polyu.hk>
2026-09-22 15:19:01 +0800

    Update

--- modules/image_gen/gpt2_train.py
+++ modules/image_gen/gpt2_train.py
@@ -456,7 +456,6 @@ class ValidationCallback(TrainerCallback):
         
         logger.info(
             f"step: {state.global_step} | "
-            f"epoch: {state.epoch:.2f} | "
             f"loss: {logs.get('loss', 'N/A'):.4f} | "
             f"grad_norm: {logs.get('grad_norm', 'N/A'):.4f} | "
             f"learning_rate: {logs.get('learning_rate', 'N/A'):.4e}"
